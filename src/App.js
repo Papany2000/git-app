a30e36f14fa1c95c088adadd414a033a2b2e469d
Papany2000 <91553924+Papany2000@users.noreply.github.com>
2021-10-02 00:21:12 +0300

    foto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Footer from './Components/Footer';
-import Heder from './Components/Heder'
-import Sidebar from './Components/Sidebar';
-import Container from './Components/Container';
+import Footer from './Components/Footer/Footer';
+import Heder from './Components/Heder/Heder'
+import Sidebar from './Components/Sidebar/Sidebar';
+import Container from './Components/Container/Container';
 const App = () => {
   return (
     <div className ='app-wrapper'>
--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import style from './Container.module.css'
+import image from './image/1.jpg'
 const Container = () =>{
    return <container className = {style.container}>
-    <p> <img src = 'https://cdn.pixabay.com/photo/2019/01/23/21/16/pixabay-3951079__340.png' alt = 'fgdfgh'/></p>
-    <p><img src = 'https://cdn.pixabay.com/photo/2017/08/02/22/25/cat-2573708__340.png' alt = 'ytn'/> Dmitry K</p>
+    <p> <img className = {style.img} src = 'https://cdn.pixabay.com/photo/2021/09/25/15/46/step-by-step-6655274__340.jpg' alt = 'fgdfgh'/></p>
+    <p><img className = {style.foto} src = {image}  alt = 'foto'/> Papany 2000</p>
     <div>
       <p>My post</p>
       <p>Отправить</p>
