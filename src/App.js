77eee9191dcffc4e89c678b3520b5a7044bb2108
Papany2000 <91553924+Papany2000@users.noreply.github.com>
2021-10-05 21:24:24 +0300

    dialog

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
+// import Dialogs from './Components/Dialogs/Dialogs.jsx';
 import Footer from './Components/Footer/Footer';
 import Heder from './Components/Heder/Heder'
 import Sidebar from './Components/Sidebar/Sidebar';
 import Container from './Components/Container/Container';
+
 const App = () => {
   return (
     <div className ='app-wrapper'>
      <Heder />
      <Sidebar />
+     <div class = "app-wrapper-container">
      <Container />
+     </div>
+     {/* <Dialogs /> */}
+     
      <Footer />
     </div>
   )
