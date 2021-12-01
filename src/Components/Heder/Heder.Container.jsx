1f146e8f6cb25fbed62bfb372229281f9bc61325
Papany2000 <91553924+Papany2000@users.noreply.github.com>
2021-12-01 20:38:31 +0300

    login

--- src/Components/Heder/Heder.Container.jsx
+++ src/Components/Heder/Heder.Container.jsx
@@ -5,17 +5,22 @@ import { connect } from 'react-redux'
 import { setAuthUserData } from '../reduc/auth-reducer';
 class HederContainer extends React.Component {
     componentDidMount() {
-        
+      axios.post(`https://social-network.samuraijs.com/api/1.0/auth/login`, {email: 'meltice.spb@gmail.com', password: 'meltice'}, {withCredentials: true}).then(response => {
+        console.log(response)
         axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-            withCredentials: true,
+            withCredentials: true, headers: {
+              'API-KEY': '2dd63b84-863f-4c04-8579-e7bb3dd375ab'
+          }
         } )
         .then(response => {
-    
+    console.log(response)
           if(response.data.resultCode === 0) {
               let {id, email, login} = response.data.data
             this.props.setAuthUserData(id, email, login)
           } 
         })
+      })
+      
     }
     
     render(){
